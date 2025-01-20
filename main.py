183519be1b42afde4098cd8afeabd4999849780e
CookieBorn <robanikin@hotmail.com>
2025-01-21 08:49:47 +1100

    H: Spin

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ pygame.init()
 from constants import *
 screen=pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 Clock=pygame.time.Clock()
-dt=0
 player=Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 
@@ -17,12 +16,14 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    dt=1
     while 0<1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         dt = Clock.tick(60) / 1000
 
--- player.py
+++ player.py
@@ -16,3 +16,12 @@ class Player(CircleShape):
         return [a, b, c]
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED*dt
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+               Player.rotate(self,dt)
+        if keys[pygame.K_RIGHT]:
+               Player.rotate(self,-dt)
