988a0a5ae2e9390b1588d1635a42f40ca7e415cb
CookieBorn <robanikin@hotmail.com>
2025-01-21 20:41:47 +1100

    N: Shots delay

--- player.py
+++ player.py
@@ -9,6 +9,7 @@ class Player(CircleShape):
     def __init__(self,x, y):
         super().__init__(x,y,PLAYER_RADIUS)
         self.rotation=0
+        self.timer=0
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -23,6 +24,7 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED*dt
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.timer-=dt
         if keys[pygame.K_LEFT]:
                Player.rotate(self,dt)
         if keys[pygame.K_RIGHT]:
@@ -31,8 +33,9 @@ class Player(CircleShape):
                Player.move(self,dt)
         if keys[pygame.K_DOWN]:
                Player.move(self,-dt)
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_SPACE] and self.timer<=0:
                Player.shoot(self)
+               self.timer=PLAYER_SHOOT_COOLDOWN
     def move(self,dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
